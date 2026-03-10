c6198e06d6f062617cb2a04d0e9be1c31eb7552d
Cheef44 <cheef44.prog@gmail.com>
2026-03-10 20:19:26 +0300

    feature(registration): реализация регистрации

---
+++ src/services/registration.py
@@ -0,0 +1,39 @@
+import asyncio
+import json
+import hashlib
+import base64
+from Crypto.Hash import SHA3_512
+from Crypto.Random import get_random_bytes
+import secrets
+
+#Класс регистрации
+class RegistrationAuthorization:
+    def __init__(self, user_login, user_name, password, user_id=None):
+        self.user_login = user_login
+        self.user_name = user_name
+        self.password = password
+        self.user_id = user_id
+    
+    #Реализация генерация соли
+    def salt_geniration(self, user_login:str, user_password:str, salt:bytes=get_random_bytes(32)):
+        user_login = bytes(user_login, "utf-8")
+        user_password = bytes(user_password, encoding="utf-8")
+        salt = salt[:len(salt)//2]+user_login+salt[len(salt)//2:]
+        sault_user_password = salt[:len(salt)//2]+user_password+salt[len(salt)//2:]
+        
+        return {"salt": salt, "salt_user_password": sault_user_password}
+    
+    #Метод регистрации
+    def registration(self):
+        salt_data = self.salt_geniration(self.user_login, self.password)
+        self.password = base64.b64encode(SHA3_512.new(salt_data["salt_user_password"]).digest()).decode('utf-8')
+        self.user_login = base64.b64encode(SHA3_512.new(bytes(self.user_login, "utf-8")).digest()).decode('utf-8')
+        requesrt = {
+            "type_request": "(REGISTRATION)",
+            "user_login": self.user_login,
+            "user_name": self.user_name,
+            "password": self.password,
+            "salt": base64.b64encode(salt_data["salt"]).decode('utf-8')
+        }
+        
+        return json.dumps(requesrt)
