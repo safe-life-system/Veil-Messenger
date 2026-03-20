24470341c0bcf2660e4d067c055451a91fb3b827
Cheef44 <cheef44.prog@gmail.com>
2026-03-20 22:27:53 +0300

    feature(backend): реализация вызова функции регистрации при нажатии кнопки

--- src/ux/backend.py
+++ src/ux/backend.py
@@ -1,5 +1,7 @@
 from qasync import asyncSlot
 from PySide6.QtCore import QObject
+import struct
+from src.services import registration
 
 #Класс бэкэнда для QML
 class Backend(QObject):
@@ -7,6 +9,11 @@ class Backend(QObject):
         super().__init__()
         self.client = client
 
-    @asyncSlot()
-    async def greet(self):
-        await self.client.send_write(b"TEST")
+    @asyncSlot(str, str, str)
+    async def registration(self, user_login, user_name, password):
+        
+        registration_data = registration.RegistrationAuthorization(user_login, user_name, password).registration().encode('utf-8')
+        
+        await self.client.send_write(struct.pack('!I', len(registration_data)))
+        await self.client.send_write(registration_data)
+        await self.client.handler_reader()
