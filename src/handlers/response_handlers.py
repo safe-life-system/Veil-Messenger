c24f9a543d4b87870f84611684c9c2f2f1de5c4d
Cheef44 <cheef44.prog@gmail.com>
2026-03-20 22:26:41 +0300

    feature(response_hanflers): реализация обработчика ответов от сервера

---
+++ src/handlers/response_handlers.py
@@ -0,0 +1,24 @@
+import asyncio, json
+from data.config import handlers_list
+from src.db import data_base_control
+
+#Клас обработки ответов об сервера
+class ResponseHandler:
+    def __init__(self, response):
+        self.response = json.loads(response)
+        self.type_response = self.response["type_response"]
+        
+    #Метод распознавания типа ответа
+    async def check_type(self):
+        if self.type_response in handlers_list.handler_list:
+            self.type_response = self.type_response.replace("(", "")
+            self.type_response = self.type_response.replace(")", "")
+            
+            return await getattr(self, self.type_response)()
+        return False
+    
+    #Метод регистрации в локлаьной БД
+    async def REGISTRATION(self):
+        registration_db = data_base_control.DataBaseControl(self.response["user_id"], self.response["user_name"], self.response["user_login"], self.response["password"], self.response["salt"])
+        registration_db.create_db()
+        registration_db.registration_db()
